ff12044f1db18e8a0038d4bd268ed502a45f45c7
adnangif <muhammadfahim010@gmail.com>
2021-07-11 14:47:33 +0600

    project complete

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-// server.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", (req, res) => {
+  const clock = new Date();
+  const time = {
+    utc: clock.toGMTString(),
+    unix: clock.getTime(),
+  };
+  res.send(time);
 });
 
+app.get("/api/:time", (req, res) => {
+  let clock;
+  let isParsable = /\w*[\s-]\w*/g;
+  if (isParsable.test(req.params.time)) {
+    clock = new Date(req.params.time);
+  } else {
+    clock = new Date(Number.parseInt(req.params.time));
+  }
+  const time = {
+    utc: clock.toGMTString(),
+    unix: clock.getTime(),
+  };
+  if (time.utc == "Invalid Date") {
+    res.send({
+      error: time.utc,
+    });
+  }
+  res.send(time);
+});
 
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT || 4000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
